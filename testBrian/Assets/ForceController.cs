5097aa6603e1a75b3247300d51180680a25b964b
Thomasina Egan <d13126034@ast-1027-m19.ict.ad.dit.ie>
2014-03-10 17:49:08 +0000

    me
    me

---
+++ testBrian/Assets/ForceController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+public class ForceController : MonoBehaviour 
+	
+{
+Vector3 force = Vector3.zero
+Vector3 velocity = Vector3.zero
+Vector3 = newDesiredTarget(enemy)			
+			float maxSpeed = 20.0f
+			float mass = 0
+			
+void Update ()
+			{
+				
+			force += Seek(target)
+				
+	Vector3acceleration = force/Mass;
+	velocity+= Acceleration*Time.deltaTime;
+Transform.position = velocity*Time.deltaTime;
+force=Vector3.zero;
+			}
+	
+	if (AudioVelocityUpdateMode.magnitude>float.eplison)
+	{
+	transform.forward=velocity.naturlize()	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+		
+		
+		
